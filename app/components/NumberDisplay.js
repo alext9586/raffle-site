3b76e81dc81dc271a9214ce1372a5eb7c53ea0fa
Alex <alext9586@users.noreply.github.com>
2016-11-20 12:51:07 -0600

    Changed number fading to use javascript instead of CSS for smoother look.

--- app/components/NumberDisplay.js
+++ app/components/NumberDisplay.js
@@ -2,7 +2,7 @@ var Raffle;
 (function (Raffle) {
     function NumberDisplay() {
         return {
-            templateUrl: "app/components/templates/NumberDisplay.html?v=1",
+            templateUrl: "app/components/templates/NumberDisplay.html?v=2",
             controller: NumberDisplayController,
             bindings: {
                 value: "@"
@@ -11,30 +11,39 @@ var Raffle;
     }
     Raffle.NumberDisplay = NumberDisplay;
     var NumberDisplayController = (function () {
-        function NumberDisplayController($timeout) {
+        function NumberDisplayController($element, $timeout) {
+            this.$element = $element;
             this.$timeout = $timeout;
-            this.$inject = ["$timeout"];
-            this.visible = false;
+            this.$inject = ["$element", "$timeout"];
         }
         Object.defineProperty(NumberDisplayController.prototype, "value", {
             get: function () {
                 return this.myValue || 0;
             },
             set: function (value) {
-                this.visible = false;
                 this.changeDisplayValue(value);
             },
             enumerable: true,
             configurable: true
         });
         NumberDisplayController.prototype.changeDisplayValue = function (value) {
-            var _this = this;
             if (this.$timeout) {
-                this.$timeout(function () {
-                    _this.myValue = value;
-                    _this.visible = true;
-                }, 110);
+                this.changeOpacity(value, 1, true);
             }
+        };
+        NumberDisplayController.prototype.changeOpacity = function (value, opacity, hide) {
+            var _this = this;
+            if (opacity < 0) {
+                this.myValue = value;
+                hide = false;
+            }
+            if (opacity > 1.0)
+                return;
+            this.$element.find(".display-value").attr("style", "opacity: " + opacity);
+            var multiplier = hide ? -0.1 : 0.1;
+            this.$timeout(function () {
+                _this.changeOpacity(value, opacity + multiplier, hide);
+            }, 10);
         };
         return NumberDisplayController;
     }());
--- app/components/NumberDisplay.ts
+++ app/components/NumberDisplay.ts
@@ -1,7 +1,7 @@
 module Raffle {
     export function NumberDisplay(): ng.IComponentOptions {
         return {
-            templateUrl: "app/components/templates/NumberDisplay.html?v=1",
+            templateUrl: "app/components/templates/NumberDisplay.html?v=2",
             controller: NumberDisplayController,
             bindings: {
                 value: "@"
@@ -10,30 +10,42 @@ module Raffle {
     }
 
     export class NumberDisplayController {
-        public $inject: string[] = ["$timeout"];
+        public $inject: string[] = ["$element", "$timeout"];
         private myValue: number;
-        private visible: boolean;
 
         public get value(): number {
             return this.myValue || 0;
         }
 
         public set value(value: number) {
-            this.visible = false;
             this.changeDisplayValue(value);
         }
         
-        constructor(private $timeout: ng.ITimeoutService) {
-            this.visible = false;
+        constructor(private $element: ng.IAugmentedJQuery, private $timeout: ng.ITimeoutService) {
+            
         }
 
         private changeDisplayValue(value: number): void {
             if(this.$timeout) {
-                this.$timeout(() => {
-                    this.myValue = value;
-                    this.visible = true;
-                }, 110);
+                this.changeOpacity(value, 1, true);
             }
+        }
+
+        private changeOpacity(value: number, opacity: number, hide?: boolean): void {
+            if(opacity < 0) {
+                this.myValue = value;
+                hide = false;
+            }
+
+            if(opacity > 1.0)
+                return;
+            
+            this.$element.find(".display-value").attr("style", `opacity: ${opacity}`);
+
+            var multiplier = hide ? -0.1 : 0.1;
+            this.$timeout(() => {
+                this.changeOpacity(value, opacity + multiplier, hide);
+            }, 10);
         }
     }
 }
